62c637c617408f88c6ad55616d95e4810733e192
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-06-30 21:01:55 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -5,18 +5,18 @@ public class Main {
         Scanner scan = new Scanner(System.in);
 
         String input = scan.nextLine();
-        char[] a = input.toCharArray();
 
         for(int i=0; i<input.length(); i++) {
-            if(a[i]>=65 && a[i]<=90) {
-                a[i] = (char)(a[i] + 32);
-                System.out.print(a[i]);
-            }
-            else if(a[i]>=97 && a[i]<=122) {
-                a[i] = (char)(a[i] - 32);
-                System.out.print(a[i]);
-            }
+
+            // 문자열(input)에 i(인덱스번호)에 해당하는 문자를 저장
+            char a = input.charAt(i);
+            if(Character.isUpperCase(a)) { // 문자가 대문자일 경우
+                System.out.print(String.valueOf(a).toLowerCase());
+            } // a 값을 소문자로 바꾼다.
+            else if(Character.isLowerCase(a)) { // 문자가 소문자일 경우
+                System.out.print(String.valueOf(a).toUpperCase());
+            } // a 값을 대문자로 바꾼다.
         }
-        
+
     }
 }
