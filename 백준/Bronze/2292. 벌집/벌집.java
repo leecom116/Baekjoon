52bdf2bc485c197d6a7bfa60e19250c73988be71
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-07-10 15:59:14 +0900

    Update 벌집.java

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,30 +1,28 @@
 import java.util.Scanner;
-// java.util.Scanner 클래스를 import 한다.
 
 public class Main {
-// 접근제어자 public으로 main class를 선언한다.
+
 	public static void main(String[] args) {
-	// 접근제어자 public으로 메모리에 상주하게 리턴값이 없이 main 함수를 선언한다.
 
 		Scanner in = new Scanner(System.in);
-		// Scanner 객체 in를 선언한다.
+		// Scanner 객체 in를 선언
  
 		int n = in.nextInt();
-		// int형 변수 n를 선언하고 값을 입력받아 저장한다.
+		
 		int count = 1;
-		// int 형 변수 count를 선언하고 값 1를 저장한다.
+		
 		int range = 2; 
-		// int 형 변수 range를 선언하고 값 2를 저장한다.
+		
 
 		if (n == 1) {
-		// 변수 n의 값이 1일때 괄호 안의 문장이 실행된다.
+		
 			System.out.print(1);
-			// 1를 출력한다.
+			
 		}
  
 		else {
 			while (range <= n) {
-			// 변수 range에 저장된 값이 변수 n에 저장된 값 이하일때 while문이 실행된다.
+			
 				range = range + (6 * count); 
 				count++;
 				/* 8 = 2 + (6) 이 처음 실행된다. count 값이 1 증가해서 2가 된다.
@@ -35,7 +33,7 @@ public class Main {
 				 */
 			}
 			System.out.print(count);
-			// count 값을 출력한다.
+			
 		}
 	}
 }
