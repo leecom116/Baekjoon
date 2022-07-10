ccee770110b52a26044ade2b5bd54a5a2f4ceebc
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-07-11 08:38:18 +0900

    Update 직각삼각형.java

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -11,9 +11,10 @@ public class Main {
             int c = sc.nextInt();
 
             if(a==0 && b==0 && c==0) {
-                break;
+                break; // 반복문을 빠져 나온다.
             }
             else if((a*a)+(b*b)==(c*c) || (b*b)+(c*c)==(a*a) || (a*a)+(c*c)==(b*b)) {
+            // 3개의 변수중에 변의 길이가 가장 긴 것이 어느 변수에 들어갈지 모르므로 3가지의 경우의 수를 지정한다.
                 System.out.println("right");
             }
             else {
