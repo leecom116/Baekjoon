0f1789059767f92eca0a48a5593625fa567b2249
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-07-13 09:14:50 +0900

    Update ACM 호텔.java

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -5,20 +5,20 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
+        int T = sc.nextInt(); // 테스트 케이스의 개수 설정
 
-        int a, b, floor;
+        int a, b; // a : 층, b : 호 수
 
 
-        for(int i=0; i<T; i++) {
+        for(int i=0; i<T; i++) { // T만큼 반복
 
-            int H = sc.nextInt(); // 6
-            int W = sc.nextInt(); // 12
-            int N = sc.nextInt(); // 10
+            int H = sc.nextInt(); // ex) 6
+            int W = sc.nextInt(); // ex) 12
+            int N = sc.nextInt(); // ex) 10
 
-            if(N%H == 0) {
-                a = H * 100;
-                b = N / H;
+            if(N % H == 0) { // 꼭대기 층 예외 처리
+                a = H * 100; 
+                b = N / H;   
             }
             else {
                 a = (N % H) * 100;
