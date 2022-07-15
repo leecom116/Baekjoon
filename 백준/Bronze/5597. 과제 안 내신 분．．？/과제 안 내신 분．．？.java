252160c1e635c5d80eba1f281e618fbd89b20d2c
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-07-15 11:59:34 +0900

    Update 과제 안 내신 분．．？.java

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean[] arr = new boolean[31];
+        boolean[] arr = new boolean[31]; 
 
         for(int i=0; i<28; i++) {
-            arr[sc.nextInt()]= true;
+            arr[sc.nextInt()]= true; // 입력된 숫자에 따라 해당 하는 인덱스 번호에 true를 
         }
 
         for(int i=1; i<arr.length; i++) {
-            if(arr[i] == false) {
+            if(arr[i] == false) { // 입력이 안 된 숫자일 경우
                 System.out.println(i);
             }
         }
