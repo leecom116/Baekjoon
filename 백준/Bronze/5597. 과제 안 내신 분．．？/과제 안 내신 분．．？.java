9882b84070bb287339803a6446fac14f8a328e03
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-07-15 11:59:53 +0900

    Update 과제 안 내신 분．．？.java

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -7,7 +7,7 @@ public class Main {
         boolean[] arr = new boolean[31]; 
 
         for(int i=0; i<28; i++) {
-            arr[sc.nextInt()]= true; // 입력된 숫자에 따라 해당 하는 인덱스 번호에 true를 
+            arr[sc.nextInt()]= true; // 입력된 숫자에 따라 해당 하는 인덱스 번호에 true를 대입
         }
 
         for(int i=1; i<arr.length; i++) {
