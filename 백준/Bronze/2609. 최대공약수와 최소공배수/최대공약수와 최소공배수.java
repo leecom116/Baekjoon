50519de2c0b562fdfa26aebec1759a5ceaa9e784
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-07-27 08:58:24 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 최대 공약수와 최소 공배수 출력하는 프로그램
+
+        int a = sc.nextInt(), b = sc.nextInt();
+        int i;
+        if(a > b) {
+            i = b;
+        } else {
+            i = a;
+        }
+
+        while(i > 0) {
+            if(a % i == 0 && b % i == 0) {
+                System.out.println(i);
+                System.out.println((a / i) * (b / i) * i);
+                break;
+            }
+            i--;
+        }
+
+    }
+}
