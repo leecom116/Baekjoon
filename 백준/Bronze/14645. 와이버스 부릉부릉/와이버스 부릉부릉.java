c9bcca50b8f67153eb86bd555300130e54e4ac37
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-08-11 09:58:13 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<K; j++) {
+                int a = sc.nextInt();
+            }
+        }
+        System.out.println("비와이");
+    }
+}
