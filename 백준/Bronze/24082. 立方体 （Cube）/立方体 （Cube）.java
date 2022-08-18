8434c1fa2b38ea3c938a7b49625ae04289341ad1
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-08-18 10:56:16 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 220 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(n * n * n);
+    }
+}
