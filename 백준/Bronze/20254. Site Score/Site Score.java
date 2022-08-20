d6114d8c0d70f31ba37b9192ffa0ac3ff9ec67a4
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-08-20 09:24:41 +0900

    [Bronze V] Title: Site Score, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println((56*a)+(24*b)+(14*c)+(6*d));
+    }
+}
