2cdce1e639982281edbeb5b7be39fd2312d58acc
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-10 23:32:02 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 220 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        int N = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+
+        int K = sc.nextInt();
+
+        for(int i=1; i<=N; i++) {
+            list.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<");
+
+        for(int i=K-1; list.size()>1; i=(i+(K-1))%list.size()) {
+
+            sb.append(list.get(i)+", ");
+            list.remove(i);
+            
+
+        }
+
+        sb.append(list.get(0)+">");
+        System.out.println(sb);
+
+
+    }
+}
