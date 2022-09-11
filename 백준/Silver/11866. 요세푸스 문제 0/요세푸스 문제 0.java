897e4f6158d27cc2fbf1eaf3571c81eb54199e5b
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-12 00:30:33 +0900

    Update 요세푸스 문제 0.java

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int count = 0;
         int N = sc.nextInt();
         List<Integer> list = new ArrayList<>();
 
