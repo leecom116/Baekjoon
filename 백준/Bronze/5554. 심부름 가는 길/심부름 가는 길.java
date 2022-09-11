b6a0e9fa077beb09340d665e5718d5a897d42677
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-11 13:07:59 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 204 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for(int i=0; i<4; i++) {
+            sum += sc.nextInt();
+        }
+
+        System.out.println(sum/60);
+        System.out.println(sum%60);
+    }
+}
