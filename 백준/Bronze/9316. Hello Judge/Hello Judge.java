f84f222a9adddafaf3e677e823b9413d7ae8d0f1
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-17 11:34:01 +0900

    [Bronze IV] Title: Hello Judge, Time: 236 ms, Memory: 18320 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for(int i=1; i<=a; i++) {
+            System.out.println("Hello World, Judge "+i+"!");
+        }
+    }
+}
