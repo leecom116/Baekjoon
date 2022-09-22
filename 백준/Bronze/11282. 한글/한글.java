4761a8be736c619c6bf164a859e43471b86d73f2
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-22 13:58:01 +0900

    [Bronze IV] Title: 한글, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // int a = sc.next().charAt(0);
+        char N = (char)(sc.nextInt()+44031);
+
+        System.out.println(N);
+    }
+}
