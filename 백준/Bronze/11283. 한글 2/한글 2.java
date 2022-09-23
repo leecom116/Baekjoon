4a3d96c6b6939d3e3a63dc027a12af84ebce26cd
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-23 13:51:02 +0900

    [Bronze IV] Title: 한글 2, Time: 216 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        int out = sc.next().charAt(0);
+
+         System.out.println(out-44031);
+     }
+}
