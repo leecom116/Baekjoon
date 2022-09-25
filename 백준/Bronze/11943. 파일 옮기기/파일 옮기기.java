3e80f7897446ffd1ad1374a7f7b2f9801cf35e4f
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-09-25 21:08:09 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 216 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+         int a = sc.nextInt(), b = sc.nextInt();
+         int c = sc.nextInt(), d = sc.nextInt();
+
+         System.out.println(Math.min(a+d, b+c));
+
+
+
+
+
+
+     }
+}
