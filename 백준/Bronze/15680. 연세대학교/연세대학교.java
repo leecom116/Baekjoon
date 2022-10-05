9bdac834862b15d23eb2fed444a7260aa094107b
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-10-05 13:25:31 +0900

    [Bronze IV] Title: 연세대학교, Time: 196 ms, Memory: 17408 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        if(N == 0) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
