9036af620afeb0273b4778223c090c8feaf59c0f
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-10-23 13:36:02 +0900

    [Bronze IV] Title: 녹색거탑, Time: 200 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        System.out.println((int) Math.pow(2,input));
+    }
+}
