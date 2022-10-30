b3df5a50cc8bf757e59007b54691d269353b10b4
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-10-30 10:16:32 +0900

    [Bronze IV] Title: 입실 관리, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++) {
+            String s = br.readLine();
+
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
