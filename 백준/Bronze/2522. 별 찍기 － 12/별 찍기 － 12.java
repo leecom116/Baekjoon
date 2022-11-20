f9b4c0c18b54e4b2ed6febe5fa2dd281a8a02211
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-11-21 08:54:03 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 216 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<N; i++) {
+            for(int j=i; j<N-1; j++) {
+                sb.append(" ");
+            }
+            for(int j=0; j<i+1; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        for(int i=0; i<N-1; i++) {
+            for(int j=0; j<i+1; j++) {
+                sb.append(" ");
+            }
+            for(int j=0; j<N-i-1; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
