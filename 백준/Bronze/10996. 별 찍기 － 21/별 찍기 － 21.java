d55ae0b491c85c25a1010d02957a318e65160f89
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-11-28 13:45:00 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 224 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<N*2; i++) {
+            for(int j=0; j<N; j++) {
+                if(i % 2 == 0 && j % 2 == 0) {
+                    sb.append("*");
+                } else if(i % 2 == 0 && j % 2 == 1) {
+                    sb.append(" ");
+                } else if(i % 2 == 1 && j % 2 == 0) {
+                    sb.append(" ");
+                } else {
+                    sb.append("*");
+                }
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
