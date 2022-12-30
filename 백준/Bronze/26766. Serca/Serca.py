344da1efc536246a954462562f00533efe2c18c9
J.CAVA <102406380+leecom116@users.noreply.github.com>
2022-12-30 23:45:25 +0900

    [Bronze V] Title: Serca, Time: 352 ms, Memory: 117468 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(n):
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
