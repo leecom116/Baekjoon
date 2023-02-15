1c91e515968b9854ffdc5e7a74c8d61194dfd1e5
J.CAVA <102406380+leecom116@users.noreply.github.com>
2023-02-15 16:53:53 +0900

    [Bronze III] Title: 자전거 속도, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/2765. 자전거 속도/자전거 속도.py
@@ -0,0 +1,16 @@
+from math import pi
+
+i = 1
+
+while True:
+    d, r, t = map(float, input().split())
+
+    if r == 0:
+        break
+    dis = d / 63360 * pi * r
+    mph = dis / t * 3600
+
+    print("Trip #%d: %.2f %.2f" % (i, dis, mph))
+
+    i += 1
+
