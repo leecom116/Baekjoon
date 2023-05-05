30f79d85d0d319e35cd0b637ac15b62c015316b5
J.CAVA <102406380+leecom116@users.noreply.github.com>
2023-05-05 18:59:11 +0900

    [Bronze III] Title: TGN, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+for i in range(N):
+    r, e, c = map(int, input().split())
+
+    if r > e - c:
+        print("do not advertise")
+    elif r < e - c:
+        print("advertise")
+    else:
+        print("does not matter")
+
